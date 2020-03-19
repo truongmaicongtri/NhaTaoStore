ad13c06600fe102290ef1e14784a7f3796f85a43
TriTruong <tri.truong.dsn@gmail.com>
2020-03-19 19:15:36 +0700

    update 6

--- js/data.js
+++ js/data.js
@@ -1,5 +1,4 @@
 var items = [{
-        id: "iphone11",
         name: "Iphone11pro",
         imageUrl: [
             "img/showcase/item1.jpg",
@@ -51,63 +50,573 @@ var items = [{
         ]
     },
     {
-        id: "iphone11a",
-        name: "Item 2",
-        imageUrl: "img/showcase/item2.jpg",
+        name: "Iphone12pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
         category: "iphone",
-    }, {
-        id: "iphone12",
-        name: "Item 3",
-        imageUrl: "img/showcase/item3.jpg",
-        category: "macbook"
-    }, {
-        id: "iphone11b",
-        name: "Item 4",
-        imageUrl: "img/showcase/item4.jpg",
-        category: "iphone"
-    }, {
-        id: "iphone11c",
-        name: "Item 5",
-        imageUrl: "img/showcase/item5.jpg",
-        category: "iphone"
-    }, {
-        id: "iphone11d",
-        name: "Item 6",
-        imageUrl: "img/showcase/item6.jpg",
-        category: "iphone"
-    }, {
-        id: "iphone11e",
-        name: "Item 7",
-        imageUrl: "img/showcase/item7.jpg",
-        category: "iphone"
-    }, {
-        id: "iphone11f",
-        name: "Item 8",
-        imageUrl: "img/showcase/item8.jpg",
-        category: "ipad"
-    }, {
-        id: "iphone11g",
-        name: "Item 9",
-        imageUrl: "img/showcase/item9.jpg",
-        category: "iphone"
-    }, {
-        id: "iphone11h",
-        name: "Item 10",
-        imageUrl: "img/showcase/item10.jpg",
-        category: "iphone"
-    }, {
-        id: "iphone11i",
-        name: "Item 11",
-        imageUrl: "img/showcase/item11.jpg",
-        category: "iphone"
-    }, {
-        id: "iphone11j",
-        name: "Item 12",
-        imageUrl: "img/showcase/item12.jpg",
-        category: "iphone"
-    }
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone13pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone11pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone11pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone11pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone11pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone11pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone11pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone11pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone11pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
+    {
+        name: "Iphone11pro",
+        imageUrl: [
+            "img/showcase/item1.jpg",
+            "img/showcase/item2.jpg",
+            "img/showcase/item3.jpg",
+            "img/showcase/item4.jpg"
+        ],
+        category: "iphone",
+        prices: [{
+                "color": "Đỏ",
+                "storage": "64GB",
+                "price": "15.100.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "64GB",
+                "price": "15.200.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "64GB",
+                "price": "15.300.000",
+            },
+            {
+                "color": "Tím",
+                "storage": "64GB",
+                "price": "15.400.000",
+            },
+            {
+                "color": "Đỏ",
+                "storage": "128GB",
+                "price": "15.500.000",
+            },
+            {
+                "color": "Vàng",
+                "storage": "128GB",
+                "price": "15.600.000",
+            },
+            {
+                "color": "Xanh",
+                "storage": "128GB",
+                "price": "15.700.000",
+            },
+            // {
+            //     "color": "Tím",
+            //     "storage": "128GB",
+            //     "price": "15.800.000",
+            // }
+        ]
+    },
 ];
 
 function getData() {
-    return items;
+    var data = [];
+
+    items.forEach(item => {
+        data.push({...item, id: item.name.toLowerCase().split(" ").join("-")});
+    });
+    return data;
 }
